70f839d64ccd790a04b212ba3c0d400e64671b2f
Klara Blašković <72518590+klblasko@users.noreply.github.com>
2021-03-07 20:14:01 +0100

    add animation logo/splash screen

--- app/src/main/java/com/example/instrukciopedijaapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/instrukciopedijaapp/SplashScreenActivity.java
@@ -5,16 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreenActivity extends AppCompatActivity {
-
+    private ImageView iv;
     Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
-
+        iv = (ImageView) findViewById(R.id.iv);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.my_animation);
+        iv.startAnimation(myanim);
 
         handler = new Handler();
         handler.postDelayed(new Runnable() {
