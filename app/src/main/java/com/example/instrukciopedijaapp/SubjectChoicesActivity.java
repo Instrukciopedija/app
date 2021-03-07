c7bc21651c2605857462bb9798116632b5beabd5
Klara Blašković <72518590+klblasko@users.noreply.github.com>
2021-03-07 23:02:57 +0100

    register and subjects

---
+++ app/src/main/java/com/example/instrukciopedijaapp/SubjectChoicesActivity.java
@@ -0,0 +1,14 @@
+package com.example.instrukciopedijaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubjectChoicesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject_choices);
+    }
+}
