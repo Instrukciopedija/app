4b9710168edf5c36947f28628123314e929640de
Anamarija Jozić <ana.jossic@gmail.com>
2021-03-08 23:46:57 +0100

    100% gotova s register_user xml! sver sam probala i na kraju mislim da sam uspjela idk

--- app/src/main/java/com/example/instrukciopedijaapp/LoginActivity.java
+++ app/src/main/java/com/example/instrukciopedijaapp/LoginActivity.java
@@ -2,9 +2,14 @@ package com.example.instrukciopedijaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,18 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
+        btn_register = (Button) findViewById(R.id.button_regInstruktor);
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegisterActivity();
+            }
+        });
+
+
+    }
+    public void openRegisterActivity(){
+        Intent register_intent = new Intent(this, RegisterUserActivity.class);
+        startActivity(register_intent);
     }
 }
