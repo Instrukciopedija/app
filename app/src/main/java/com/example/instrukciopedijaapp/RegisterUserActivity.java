ffd59a930b94043a0bfd0f131887b87cfcba6c8f
Anamarija Jozić <ana.jossic@gmail.com>
2021-03-09 23:43:20 +0100

    BACK BUTTON RADI, RADI I REGISTRIRAJ SE KAO INSTRUKTOR ALI JE SADA REGISTERUSERACTIVITY ZA INSTRUKTORA im sowwy

--- app/src/main/java/com/example/instrukciopedijaapp/RegisterUserActivity.java
+++ app/src/main/java/com/example/instrukciopedijaapp/RegisterUserActivity.java
@@ -2,7 +2,9 @@ package com.example.instrukciopedijaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterUserActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class RegisterUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+    }
+    public void back_btn(View view){
+        startActivity(new Intent(RegisterUserActivity.this, LoginActivity.class));
     }
 }
